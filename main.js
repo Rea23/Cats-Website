ad9e14878113e9020759de49f77f89e30f3d50c9
Rea23 <36733245+Rea23@users.noreply.github.com>
2018-09-18 19:46:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         
         document.getElementById("main-column").appendChild(clone_article);
 
-        //add new comment button to main column
+        //add new comment button into main column
         var element_commentBtn = document.getElementsByClassName("comment-article-button")[0];
         var clone_commentBtn = element_commentBtn.cloneNode(true);
         document.getElementById("main-column").appendChild(clone_commentBtn);
@@ -44,6 +44,46 @@ document.addEventListener("DOMContentLoaded", () => {
             commentBtn.remove();
             article.remove();
         }
+    }
+
+    const commentBtns = document.querySelectorAll(".comment-article-button");
+    for(const commentBtn of commentBtns) {
+        commentBtn.addEventListener("click", Add_Comment);
+    }
+
+    function Add_Comment() {
+        var comment_person;
+        var comment_content;
+        comment_person = window.prompt("Unesite svoje ime:", "");
+        comment_content = window.prompt("Unesite svoj komentar:", ""); 
+
+        //add new comment into comment section
+        var element_comment_person = document.getElementsByTagName("p")[0];
+        var clone_comment_person = element_comment_person.cloneNode(true);
+        var element_comment_content = document.getElementsByTagName("p")[1];
+        var clone_comment_content = element_comment_content.cloneNode(true);
+
+        //set heading value to input value
+        clone_comment_person.textContent = comment_person;
+        clone_comment_content.textContent = comment_content;
+
+        document.getElementById("comments-section").appendChild(clone_comment_person);
+        document.getElementById("comments-section").appendChild(clone_comment_content);
+    }
+
+    document.getElementById("links-section-add-button").onclick = function() {Add_Link()};
+
+    function Add_Link() {
+        var link_name;
+        var link_url;
+        link_name = window.prompt("Unesite naziv linka:", "");
+        link_url = window.prompt("Unesite url linka:", "");
+
+        //add link into links section
+        var element_link_name = document.getElementsByClassName("links-section-link")[0];
+        var clone_link = element_link_name.cloneNode(true);
+
+        document.getElementById("links-section").appendChild(clone_link);
     }
 
 });
