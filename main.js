fd7051637c609c37685622ffa706cf120cd87f57
Rea23 <36733245+Rea23@users.noreply.github.com>
2018-09-18 16:45:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("add-article-button").onclick = function() {Add_Article()};
+
+    function Add_Article() {
+        var picture_url;
+        var article_heading;
+        picture_url = window.prompt("Unesite url slike:", "");
+        article_heading = window.prompt("Unesite naslov clanka:", "");
+    
+        //add new article to main column
+        var element_article = document.getElementsByClassName("article-container")[0];
+        var clone_article = element_article.cloneNode(true);
+        document.getElementById("main-column").appendChild(clone_article);
+
+        //add new comment button to main column
+        var element_commentBtn = document.getElementsByClassName("comment-article-button")[0];
+        var clone_commentBtn = element_commentBtn.cloneNode(true);
+        document.getElementById("main-column").appendChild(clone_commentBtn);
+    }
+
+    const deleteBtns = document.querySelectorAll(".article-container > .main-column-delete-button");
+    for(const deleteBtn of deleteBtns) {
+        deleteBtn.addEventListener("click", Delete_Article);
+    }
+
+    function Delete_Article(e) {
+        const deleteBtn = e.currentTarget;
+        const article = deleteBtn.parentElement;
+        const article_title = article.querySelector("h3");
+
+        //delete article and comment button if confirmed
+        if(confirm("Sigurno zelite izbrisati: " + article_title.textContent)) {
+            const commentBtn = article.nextElementSibling;
+            commentBtn.remove();
+            article.remove();
+        }
+    }
+
+});
